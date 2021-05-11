0022e3a40db62bf2d88d4908bc92d99a9842b082
Amaan-Farooq <69904203+Amaan-Farooq@users.noreply.github.com>
2021-05-11 16:13:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const fs=require('fs')
 const path=require('path')
 const app=express() 
-const port=8080
+const port=8080 || process.env.PORT
 const axios = require('axios')
 
 var latitude,longitude
