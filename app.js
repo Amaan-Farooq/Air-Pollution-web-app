999f5f9cfa086155cc1ecb533eb63784b63f025b
Mohd Salman <salman0ansariii@gmail.com>
2021-05-11 17:40:40 +0530

    Nothing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const fs=require('fs')
 const path=require('path')
 const app=express() 
-const port=8080 || process.env.PORT
+const port= process.env.PORT || 8080
 const axios = require('axios')
 
 var latitude,longitude
