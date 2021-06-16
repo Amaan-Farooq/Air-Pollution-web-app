dcaff02b26149b00b66c9d00d75d747d52ba5c7c
Mohd Salman Ansari <salman0ansariii@gmail.com>
2021-06-16 11:26:09 +0530

    rewritten whole app

--- app.js
+++ app.js
@@ -1,74 +1,93 @@
-const express=require('express')
-const fs=require('fs')
-const path=require('path')
-const app=express() 
-const port= process.env.PORT || 8080
-const axios = require('axios')
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+require('dotenv').config()
 
-var latitude,longitude
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day} ${date} ${month} ${year}`;
+}
 
-app.use('/static',express.static('static'))
-app.use(express.urlencoded())
+
+const app = express();
+
+app.use('/static',express.static('static'));
+app.use(express.urlencoded({extended: true})); 
 
 // PUG SPECIFIC STUFF
 app.set('view engine','pug')// Set the template engine as pug
 app.set('views',path.join(__dirname,'views'))// Set the views directory
+app.use(express.static(path.join(__dirname, 'views')));
+
 
 // END POINTS
-app.get('/',(req,res)=>{
-    res.status(200).render('index.pug')
-})
-app.get('*',(req,res)=>{
-    res.status(200).render('index.pug')
-})
- 
-app.post('/submit',(req,res)=>{
-    city = req.body.name
-    // console.log(city)
-    link1 = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=79b1287a90d94822bf29b3bff1f25936` 
-    axios.get(link1)
-    .then(response=>{
-            latitude = response.data.results[0].geometry.lat
-            longitude = response.data.results[0].geometry.lng
-            // console.log(latitude,longitude)
+app.get('/',async (req,res)=>{
+    let now = new Date();
+    let date = dateBuilder(now);
+    const params = {'Date':date}
+    res.status(200).render('index.pug',params)
 
-            link2= `http://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=d5d356c2-7819-4791-9082-0b8b5a3ef8f2`
-            axios.get(link2)
-            .then(response =>{
-                // console.log(response.data.data.current)
-             aqi = response.data.data.current.pollution.aqius
-             major_pollutant = response.data.data.current.pollution.mainus
-            //  console.log(aqi,major_pollutant)
-            const params = {'AQI':aqi,'Pollutant':major_pollutant,'City':city}
-            if(aqi <= 50)
-            {
-                res.status(200).render('page1.pug',params)
-            }
-            else if (aqi > 50 && aqi <=100)
-            {
-                res.status(200).render('page2.pug',params)
-            }
-            else if (aqi > 100 && aqi <=150)
-            {
-                res.status(200).render('page3.pug',params)
-            }
-            else if (aqi > 150 && aqi <=200)
-            {
-                res.status(200).render('page4.pug',params)
-            }
-            else if (aqi > 200 && aqi <=250)
-            {
-                res.status(200).render('page5.pug',params)
-            }
-            else
-            {
-                res.status(200).render('page6.pug',params)
-            }
-    })
-    })
-    
-    
 })
-app.listen(port,()=>{
-    console.log(`The application started successsfully on port ${port}`)
+
+app.post('/air',async(req,res)=>{
+    let city = req.body.name
+    console.log(city);
+    try {
+        const link1 = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${process.env.API_KEY}` 
+        let response = await axios.get(link1)
+        let latitude = response.data.results[0].geometry.lat
+        let longitude = response.data.results[0].geometry.lng
+        console.log(latitude, longitude);
+        const link2 = `http://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=${process.env.API_KEY2}`
+        response = await axios.get(link2)
+        let aqi = response.data.data.current.pollution.aqius
+        let major_pollutant = response.data.data.current.pollution.mainus
+        let CITY = response.data.data.city + ', ' +response.data.data.country
+        /// day and date
+        let now = new Date();
+        let date = dateBuilder(now);
+
+        
+        const params = {'AQI':aqi,'Pollutant':major_pollutant,'City':CITY, 'Date':date}
+        
+        switch (true) {
+            case (aqi <= 50):
+                res.status(200).render('green.pug',params)
+                break;
+            case (aqi > 50 && aqi <= 100):
+                res.status(200).render('yellow.pug',params)
+                break;
+            case (aqi > 100 && aqi <= 150):
+                res.status(200).render('orange.pug',params)
+                break;
+            case (aqi > 150 && aqi <=200):
+                res.status(200).render('red.pug',params)
+                break;
+            case (aqi > 150 && aqi <=250):
+                res.status(200).render('purple.pug',params)
+                break;
+            default:
+                res.status(200).render('dark.pug',params)
+                break;
+        }
+    } catch (error) {console.log(error)}
+})
+
+app.get('*',(req,res)=>{
+    let now = new Date();
+    let date = dateBuilder(now);
+    const params = {'Date':date}
+    res.status(200).render('index.pug',params)
+})
+
+app.listen(PORT,()=>{
+    console.log(`The application started successsfully on port ${PORT}`)
 })
